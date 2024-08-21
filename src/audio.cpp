5641cb954a6e0e581ed1665389108e88b58c8a82
rxn7 <rotthin_dev@protonmail.com>
2024-08-21 20:35:53 +0200

    New move sounds

--- src/audio.cpp
+++ src/audio.cpp
@@ -1,13 +1,14 @@
 #include "audio.h"
 #include <iostream>
-#include <SFML/Audio/SoundBuffer.hpp>
-#include <unordered_map>
 
 sf::Sound Audio::s_sound;
-std::unordered_map<Sound, sf::SoundBuffer> Audio::s_soundBuffers;
+std::unordered_map<Sound, std::vector<sf::SoundBuffer>> Audio::s_soundBuffers;
 
 void Audio::init() {
-	addSoundBuffer(Sound::Move, "assets/sounds/move.ogg");
+	addSoundBuffer(Sound::Move, "assets/sounds/move1.ogg");
+	addSoundBuffer(Sound::Move, "assets/sounds/move2.ogg");
+	addSoundBuffer(Sound::Move, "assets/sounds/move3.ogg");
+	addSoundBuffer(Sound::Move, "assets/sounds/move4.ogg");
 	addSoundBuffer(Sound::Capture, "assets/sounds/take.wav");
 	addSoundBuffer(Sound::Check, "assets/sounds/check.ogg");
 	addSoundBuffer(Sound::Checkmate, "assets/sounds/checkmate.ogg");
@@ -15,25 +16,32 @@ void Audio::init() {
 }
 
 void Audio::playSound(Sound type) {
-	const std::unordered_map<Sound, sf::SoundBuffer>::iterator it = s_soundBuffers.find(type);
+	const std::unordered_map<Sound, std::vector<sf::SoundBuffer>>::iterator it = s_soundBuffers.find(type);
 
-	if (it == s_soundBuffers.end()) {
-		std::cerr << "\e[1;31mSoundBuffer for SoundType " << (int)type << " not found!\e[0m\n";
+	if(it == s_soundBuffers.end()) {
+		std::cerr << "\e[1;31mSoundBuffer for SoundType " <<(int)type << " not found!\e[0m\n";
 		return;
 	}
 
-	const sf::SoundBuffer &buf = it->second;
+	const std::size_t idx = it->second.size() == 1 ? 0 : rand() % it->second.size();
+	const sf::SoundBuffer &buf = it->second[idx];
 
 	s_sound.setBuffer(buf);
-	s_sound.setPitch((9 + (rand() % 3)) / 10.0f);
+	s_sound.setPitch((9 +(rand() % 3)) / 10.0f);
 	s_sound.play();
 }
 
 void Audio::addSoundBuffer(Sound type, const std::string_view path) {
 	sf::SoundBuffer buf;
-	if (!buf.loadFromFile(std::string(path))) {
+	if(!buf.loadFromFile(std::string(path))) {
 		std::cerr << "\e[1;31mFailed to load sound " << path << "!\e[0m\n";
 		return;
 	}
-	s_soundBuffers.insert({type, buf});
+
+	std::unordered_map<Sound, std::vector<sf::SoundBuffer>>::iterator it = s_soundBuffers.find(type);
+	if(it != s_soundBuffers.end()) {
+		it->second.push_back(buf);
+	} else {
+		s_soundBuffers.insert({type, {buf}});
+	}
 }
--- src/audio.h
+++ src/audio.h
@@ -2,7 +2,9 @@
 
 #include <cstdint>
 #include <SFML/Audio/Sound.hpp>
+#include <SFML/Audio/SoundBuffer.hpp>
 #include <string>
+#include <vector>
 #include <unordered_map>
 
 enum class Sound : std::uint8_t {
@@ -22,6 +24,6 @@ class Audio {
 	static void addSoundBuffer(Sound type, const std::string_view path);
 
   private:
-	static std::unordered_map<Sound, sf::SoundBuffer> s_soundBuffers;
+	static std::unordered_map<Sound, std::vector<sf::SoundBuffer>> s_soundBuffers;
 	static sf::Sound s_sound;
 };
